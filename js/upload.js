7bf06de4a15123f47d033087e16626f285d2edb9
amit <amitush27@gmail.com>
2021-10-30 00:42:59 +0300

    add facebook upload

--- js/upload.js
+++ js/upload.js
@@ -5,3 +5,69 @@ function downloadCanvas(link) {
     link.href = data;
     link.download = 'meme';
 }
+
+
+
+// function onImgInput(ev) {
+//     loadImageFromInput(ev, renderImg)
+// }
+
+function loadImageFromInput(ev, onImageReady) {
+    document.querySelector('.share-container').innerHTML = ''
+    var reader = new FileReader()
+
+    reader.onload = function (event) {
+        var img = new Image()
+        img.onload = onImageReady.bind(null, img)
+        img.src = event.target.result
+        // gImg = img
+    }
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+
+// function renderImg(img) {
+//     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+// }
+
+
+function uploadImg() {
+    
+    const imgDataUrl = gCanvas.toDataURL("image/jpeg");
+
+    // A function to be called if request succeeds
+    function onSuccess(uploadedImgUrl) {
+        const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+        document.querySelector('.user-msg').innerText = `Your photo is available here: ${uploadedImgUrl}`
+
+        document.querySelector('.share-container').innerHTML = `
+        <a class="btn" href="https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}" 
+        title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+           Share   
+        </a>`
+    }
+    doUploadImg(imgDataUrl, onSuccess);
+}
+
+function doUploadImg(imgDataUrl, onSuccess) {
+
+    const formData = new FormData();
+    formData.append('img', imgDataUrl)
+
+    fetch('//ca-upload.com/here/upload.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(res => res.text())
+    .then((url)=>{
+        console.log('Got back live url:', url);
+        onSuccess(url)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+}
+
+
+
+
